ecb5231c393dac230b0b16be7b95f9b8d763d302
Leslie Diaz <lmdiaz36@gmail.com>
2021-06-21 19:55:22 -0500

    con parametros de body :D

--- back/servidor-correo.js
+++ back/servidor-correo.js
@@ -16,44 +16,32 @@ app.listen(3000, () => {
   console.log("The server started on port 3000");
 });
 
-const mailOptions = {
-  from: `"Registro de Cursos", "registro.cursosweb@mail.com"`,
-  to: `lmdiaz36@gmail.com`,
-  subject: "Confirmacion de Registro",
-  html: "<p>Gracias por inscribirte a nuestro curso de ____ el costo del mismo es de $______</p>"
-};
-
 // define a sendmail endpoint, which will send emails and response with the corresponding status
 app.post("/sendmail", (req, res) => {
   console.log("request came");
   let user = req.body;
-  sendMail(user, (err, info) => {
-    if (err) {
-      console.log(err);
-      res.status(400);
-      res.send({ error: "Failed to send email" });
-    } else {
-      console.log("Email has been sent");
-      res.send(info);
-      res.status(200);
+  const mailOptions = {
+    from: `"Registro de Cursos", "registro.cursosweb@mail.com"`,
+    to: user.correo,
+    subject: "Confirmacion de Registro",
+    html: `<p>Gracias por inscribirte a nuestro curso de ${user.curso} el costo del mismo es de $${user.precio}</p>`
+  };
+  const transport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "mailpruebas46@gmail.com",
+      pass: "zdmmitiymhbonzvl"
     }
   });
+  transport.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(400);
+      return console.log(error);
+    }
+    console.log('Mensaje Enviado: %s', info.messageId);
+    res.status(200);
+    return "Enviado";
+  });
 });
-
-const sendMail = (user, callback) => {
-    const transport = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: "mailpruebas46@gmail.com",
-        pass: "zdmmitiymhbonzvl"
-      }
-    });
-    transport.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return console.log(error);
-      }
-      console.log('Mensaje Enviado: %s', info.messageId);
-    });
-  }
